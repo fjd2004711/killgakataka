f4b7c2291e444c486b651308ab239e0bfa422b89
xiaomi250 <96307726+fjd2004711@users.noreply.github.com>
2022-10-16 12:47:57 +0800

    v1.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
@@ -38,12 +36,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.linkLabel3 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
             this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -51,35 +45,13 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(317, 204);
+            this.button1.Location = new System.Drawing.Point(45, 136);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 52);
             this.button1.TabIndex = 0;
             this.button1.Text = "关闭控制";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(317, 343);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 52);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "注销";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(317, 277);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(143, 52);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "恢复控制";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
@@ -95,7 +67,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(280, 79);
+            this.label2.Location = new System.Drawing.Point(311, 79);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(112, 17);
             this.label2.TabIndex = 4;
@@ -105,7 +77,7 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.linkLabel1.Location = new System.Drawing.Point(538, 455);
+            this.linkLabel1.Location = new System.Drawing.Point(176, 220);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(149, 20);
             this.linkLabel1.TabIndex = 5;
@@ -118,18 +90,18 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(732, 458);
+            this.label3.Location = new System.Drawing.Point(404, 220);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 17);
             this.label3.TabIndex = 6;
-            this.label3.Text = "v1.0";
+            this.label3.Text = "v1.1";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(309, 455);
+            this.label4.Location = new System.Drawing.Point(45, 220);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(114, 20);
             this.label4.TabIndex = 7;
@@ -147,7 +119,7 @@
             // 
             this.linkLabel2.AutoSize = true;
             this.linkLabel2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.linkLabel2.Location = new System.Drawing.Point(457, 455);
+            this.linkLabel2.Location = new System.Drawing.Point(404, 168);
             this.linkLabel2.Name = "linkLabel2";
             this.linkLabel2.Size = new System.Drawing.Size(65, 20);
             this.linkLabel2.TabIndex = 9;
@@ -155,51 +127,20 @@
             this.linkLabel2.Text = "作者博客";
             this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(544, 79);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(243, 153);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "功能介绍\r\n1、关闭控制就是解除伽卡他卡教师机控制。\r\n\r\n2、当点击结束控制按钮时，伽卡他卡程序\r\n     又重新启动，需要点击注销按钮注销计\r\n     算" +
-    "机，然后重新登陆账户，再运行本软\r\n     件关闭控制。\r\n\r\n3、恢复控制就是恢复被控制状态。";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.ForeColor = System.Drawing.Color.Green;
-            this.label8.Location = new System.Drawing.Point(45, 225);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(212, 170);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "已知问题\r\n在被控制时使用本软件解除控制会出现\r\n桌面图标和任务栏消失的情况。\r\n\r\n解决方法：注销计算机，重新登\r\n录账户，在还没有没被控制的瞬间运行\r\n本软件" +
-    "结束伽卡他卡控制。一定要在被\r\n控制之前运行关闭控制，否则还会出现\r\n上述问题，若出现上述问题，重新执行\r\n上面步骤即可！";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label9.Location = new System.Drawing.Point(45, 139);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(415, 38);
-            this.label9.TabIndex = 13;
-            this.label9.Text = "使用方法\r\n在还没有没控制前运行此软件。被控制时运行本软件请看已知问题";
-            // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label10.Location = new System.Drawing.Point(545, 352);
+            this.label10.Location = new System.Drawing.Point(210, 171);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(200, 17);
+            this.label10.Size = new System.Drawing.Size(188, 17);
             this.label10.TabIndex = 14;
-            this.label10.Text = "↓若有任何问题，请到作者博客留言↓";
+            this.label10.Text = "若有任何问题，请到作者博客留言";
             // 
             // linkLabel3
             // 
             this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(545, 312);
+            this.linkLabel3.Location = new System.Drawing.Point(367, 136);
             this.linkLabel3.Name = "linkLabel3";
             this.linkLabel3.Size = new System.Drawing.Size(56, 17);
             this.linkLabel3.TabIndex = 15;
@@ -207,21 +148,10 @@
             this.linkLabel3.Text = "开源地址";
             this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
             // 
-            // linkLabel4
-            // 
-            this.linkLabel4.AutoSize = true;
-            this.linkLabel4.Location = new System.Drawing.Point(545, 378);
-            this.linkLabel4.Name = "linkLabel4";
-            this.linkLabel4.Size = new System.Drawing.Size(56, 17);
-            this.linkLabel4.TabIndex = 16;
-            this.linkLabel4.TabStop = true;
-            this.linkLabel4.Text = "留言地址";
-            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(545, 277);
+            this.label6.Location = new System.Drawing.Point(210, 136);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(142, 17);
             this.label6.TabIndex = 17;
@@ -231,14 +161,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(810, 484);
+            this.ClientSize = new System.Drawing.Size(482, 258);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.linkLabel4);
             this.Controls.Add(this.linkLabel3);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -246,8 +172,6 @@
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "伽卡他卡解除控制工具";
@@ -259,8 +183,6 @@
         #endregion
 
         private Button button1;
-        private Button button2;
-        private Button button3;
         private Label label1;
         private Label label2;
         private LinkLabel linkLabel1;
@@ -268,12 +190,8 @@
         private Label label4;
         private Label label5;
         private LinkLabel linkLabel2;
-        private Label label7;
-        private Label label8;
-        private Label label9;
         private Label label10;
         private LinkLabel linkLabel3;
-        private LinkLabel linkLabel4;
         private Label label6;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -23,34 +23,6 @@ namespace gakataka
             MessageBox.Show("关闭完成！若重新被控制请点击注销按钮注销账户，重新登陆后再点击关闭控制。", "关闭提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
-            p.StartInfo.FileName = "CMD.exe";//要启动的应用程序
-            p.StartInfo.UseShellExecute = false;//不使用操作系统shell启动
-            p.StartInfo.RedirectStandardInput = true; //接受来自调用程序的输入信息
-            p.StartInfo.RedirectStandardOutput = true;//允许输出信息
-            p.StartInfo.RedirectStandardError = true;//允许输出错误
-            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
-            p.Start();//启动程序
-            p.StandardInput.WriteLine("shutdown /l");//向CMD窗口发送输入命令
-            p.StandardInput.AutoFlush = true;//自动刷新
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
-            p.StartInfo.FileName = "CMD.exe";//要启动的应用程序
-            p.StartInfo.UseShellExecute = false;//不使用操作系统shell启动
-            p.StartInfo.RedirectStandardInput = true; //接受来自调用程序的输入信息
-            p.StartInfo.RedirectStandardOutput = true;//允许输出信息
-            p.StartInfo.RedirectStandardError = true;//允许输出错误
-            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
-            p.Start();//启动程序
-            p.StandardInput.WriteLine(@"""C:\Program Files (x86)\gakataka\Student\Student.exe""");//向CMD窗口发送输入命令
-            p.StandardInput.AutoFlush = true;//自动刷新
-        }
-
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start("explorer.exe", "https://biuioi.com//archives//35");
